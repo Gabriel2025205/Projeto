96783e303613af9aa02cd42efaadc006b7925015
Fabio Farias <fabio.farias@terceirizados.medgrupo.com.br>
2025-02-09 20:32:06 -0300

    novo teste

---
+++ novo projeto/index.js
@@ -0,0 +1,11 @@
+function enviar(){
+    document.getElementById('form')
+    document.getElementById('flex')
+    document.getElementsByClassName('flex')
+    
+    if( flex || form == null){
+        alert('Erro')
+    } else if (flex || form != null){
+        alert('Tudo certo')
+    }  
+} 
